2dc0ead699a6903d729323fd248ed5f0558771d1
MONSTER B <dvnv36@mail.ru>
2023-03-13 23:35:38 +0300

    commit

--- src/main/java/com/kursovaya1/kursovaya1/Service/EmployeeService.java
+++ src/main/java/com/kursovaya1/kursovaya1/Service/EmployeeService.java
@@ -6,8 +6,8 @@ import com.kursovaya1.kursovaya1.exception.EmployeeNotFoundException;
 import com.kursovaya1.kursovaya1.exception.EmployeeStorageIsFullException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -15,53 +15,58 @@ public class EmployeeService {
 
     private static final int MAX_EMPLOYEES_COUNT = 5;
 
-    private final List<Employee> employees = new ArrayList<>();
+    private final Map<Integer, Employee> employeeByHashCode = new HashMap<>();
 
     public Employee add(String firstName, String lastName) {
 
-        if (employees.size() == MAX_EMPLOYEES_COUNT) {
-            throw new EmployeeStorageIsFullException("Массив сотрудников переполнен");
+        if (employeeByHashCode.size() == MAX_EMPLOYEES_COUNT) {
+            throw new EmployeeStorageIsFullException("Хранилище сотрудников переполнено");
+
         }
 
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
-            throw new EmployeeAlreadyAddedException("В массиве уже есть такой сотрудник.");
+        int employeeHashCode = employee.hashCode();
+
+        if (employeeByHashCode.containsKey(employeeHashCode)) {
+            throw new EmployeeAlreadyAddedException("В Хранилище уже есть такой сотрудник.");
+
         }
-        employees.add(employee);
+        employeeByHashCode.put(employeeHashCode, employee);
+
         return employee;
     }
 
 
     public Employee find(String firstName, String lastName) {
-        Employee employee = null;
-        for (Employee e : employees) {
-            if (e != null && firstName.equals(e.getFirstName()) && lastName.equals(e.getLastName())) {
-                employee = e;
-            }
-        }
 
-        if (employee == null) {
-            throw new EmployeeNotFoundException("Сотрудник не найден.");
-        }
+        int employeeHashCode = getEmployeeKey(firstName, lastName);
+        Employee employee = employeeByHashCode.get(employeeHashCode);
 
+        presentCheck(employee);
         return employee;
-
     }
 
     public Employee remove(String firstName, String lastName) {
-        Employee employee = find(firstName, lastName);
+        //  Employee employee = find(firstName, lastName);
+        int employeeHashCode = getEmployeeKey(firstName, lastName);
+        Employee employee = employeeByHashCode.remove(employeeHashCode);
 
-
-        for (Employee e : employees) {
-            if (e.equals(employee)) {
-                return e;
-            }
-        }
-
+        presentCheck(employee);
         return employee;
     }
 
     public List<Employee> getAll() {
-        return employees;
+        return employeeByHashCode.values().stream().collect(Collectors.toList());
+    }
+
+    private int getEmployeeKey(String firstName, String lastName) {
+        return Objects.hash(firstName, lastName);
+    }
+
+    private void presentCheck(Employee employee) {
+        if (employee == null) {
+            throw new EmployeeNotFoundException("Сотрудник не найден");
+        }
+
     }
 }
